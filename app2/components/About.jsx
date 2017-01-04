0221ac7846667651b153aa5910b0e728390352b7
dongping <dongping@gome.com.cn>
2017-01-04 14:51:45 +0800

    a

--- app2/components/About.jsx
+++ app2/components/About.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 var About = React.createClass({
+    componentDidMount:function(){
+        alert("mount")
+    },
+    componentWillUnmount:function(){
+        alert("unmount")
+    },
+    getInitialState:function(){
+        alert("ini");
+        return {}
+    },
     render:function(){
         return (
             <div>
